57e633adba06c741f1856c7e18a1498ab605162c
Teraesa  Metivier <teraesam@yahoo.com>
2022-02-06 12:08:40 -0500

    adding questions 1 to 3

--- index.js
+++ index.js
@@ -26,7 +26,18 @@ const animals = [
  * getTotalCount(animals); //> 28
  * getTotalCount([]); //> 0 // returns 0 if the input array is empty
  */
-function getTotalCount(animals) {}
+//1. create a variable to store total count of animals
+//2. loop through the array of objects to get the key count and value to add up
+//3. push total count of animals into the variable created
+//4. return/console.log the totalcount
+function getTotalCount(animals) {
+  let animalTotal = 0;
+  for (let animal of animals) {
+    animalTotal += animal.count;
+  }
+
+  return animalTotal;
+}
 
 /**
  * FUNCTION DESCRIPTION
@@ -40,7 +51,16 @@ function getTotalCount(animals) {}
  * getAllKinds(animals); //> ["Pig", "Cow", "Chicken", "Horse", "Dog", "Cat"]
  * getAllKinds([]); //> [] // returns empty array if input array is empty
  */
-function getAllKinds(animals) {}
+//return the type of animal => the key kind
+//return kind in an array of strings
+//the length of the array "kind" shoudld ===  the length of the inputted array in the function
+function getAllKinds(animals) {
+  let arrayOfAnimals = [];
+  for (let animal of animals) {
+    arrayOfAnimals.push(animal.kind);
+  }
+  return arrayOfAnimals;
+}
 
 /**
  * FUNCTION DESCRIPTION
@@ -59,7 +79,19 @@ function getAllKinds(animals) {}
   ];
  * filterByCountMinimum([], 3); //> [] // returns empty array if input array is empty
  */
-function filterByCountMinimum(animals, minimum) {}
+//return an array of objects => where the number of animals "count" is greater than or equal too the minimum value inputted =? smallest number/animal count in the array of objects
+//if the count of animlas is higher that the minimum count of animals then array could be an empyu array and should return this
+//1. declare a varible to hold min count
+function filterByCountMinimum(animals, minimum) {
+  let value = []; //>= miniumum value inputted
+  for (let i = 0; i < animals.length; i++) {
+    let animal = animals[i];
+    if (animal.count >= minimum) {
+      value.push(animal);
+    }
+  }
+  return value;
+}
 
 /**
  * FUNCTION DESCRIPTION
@@ -73,7 +105,36 @@ function filterByCountMinimum(animals, minimum) {}
  * getMostCommonAnimal(animals); //> { kind: "Chicken", count: 11 }
  * getMostCommonAnimal([]); //> null // returns null if the input is empty
  */
-function getMostCommonAnimal(animals) {}
+//return a single object = has the hight count/or the animals that's their the most compared to all other counts of animals
+//if there are multiple animals with the same count then return the animals that appears first
+//1. create variable to store the highest count
+//2. loopp through to find the highest count
+//3. create conditional after looping to find highest count
+function getMostCommonAnimal(animals) {
+  if (animals.length === 0) {
+    return null;
+  }
+  //return highest count
+  let biggestCount = {};
+  let mostFrequentCount = {};
+  let number = -Infinity;
+
+  for (let i = 0; i < animals.length; i++) {
+    console.log("hfioefoeiwfhgoehgfoefh", animals);
+    if (animals[i].count > number) {
+      return (biggestCount = animals[i]);
+    }
+    // return `${biggestCount}``${mostFrequentCount}`;
+  }
+  // for (let i = 0; i < animals.length; i++) {
+  //   if (animals[i].count > biggestCount) {
+  //     biggestCount = animals[i];
+  //   }
+  // }
+
+  //return most refrequent animal that appears firs if its more than one with came count
+  return biggestCount;
+}
 
 // Do not change anything below this line.
 module.exports = {
